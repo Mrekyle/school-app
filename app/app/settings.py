04a96214eb7d6e6bae540575507262b5ed1cf742
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-03-30 17:25:41 +0000

    Added toast/alert messages for user interface

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,6 +119,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
+# MESSAGE OPTIONS
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+# MESSAGE_TAGS = {
+#     messages.DEBUG: 'alert-info',
+#     messages.INFO: 'alert-info',
+#     messages.SUCCESS: 'alert-success',
+#     messages.WARNING: 'alert-warning',
+#     messages.ERROR: 'alert-danger',
+# }
 
 # STORAGE AND DATABASE OPTIONS
 
